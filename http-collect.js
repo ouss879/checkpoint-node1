ef55b5284972d2b77d5e7a5660afefc243d6be24
Ouss879 <Oussama_yeddou@yahoo.fr>
2020-12-25 09:07:25 +0100

    first commit

---
+++ http-collect.js
@@ -0,0 +1,13 @@
+const http=require("http")
+const url=process.argv[2]
+const bl = require("bl")
+
+http.get(url,  (response) => {
+  response.pipe(bl( (err, data)=> {
+    if (err)
+      return console.error(err)
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
+})
